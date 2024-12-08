6f75acf15d90f650a76bb89270b4c9c2f474914b
sagar_patil <sagarpatil35137@gmail.com>
2024-12-09 01:30:34 +0530

    Added images for projects

--- src/app/components/my-projects/my-projects.component.ts
+++ src/app/components/my-projects/my-projects.component.ts
@@ -6,14 +6,14 @@ import { RouterModule } from '@angular/router';
   selector: 'app-my-projects',
   imports: [CommonModule, RouterModule],
   templateUrl: './my-projects.component.html',
-  styleUrl: './my-projects.component.scss'
+  styleUrl: './my-projects.component.scss',
 })
 export class MyProjectsComponent {
 
   public projectsDetails = [
     { image: '/assets/images/blog-web.png', title: 'Blogs web app', tools: 'Angular 16, Tailwind, NestJs, MongoDB', link: 'https://coding-blogs-seven.vercel.app/blogs' },
     { image: '/assets/images/gym-web.jpg', title: 'Gym web app', tools: 'Angular 19, Tailwind', link: 'https://fitnessclub-project.web.app/' },
-    { image: '', title: 'Music web app', tools: 'Angular 14, Bootstrap', link: 'https://sagarpatil-17.github.io/freshmusic/' },
+    { image: '/assets/images/gallery-web.png', title: 'Image gallery web', tools: 'Angular 16, Bootstrap', link: 'https://snapify-cyan.vercel.app/' },
   ]
 
 }
