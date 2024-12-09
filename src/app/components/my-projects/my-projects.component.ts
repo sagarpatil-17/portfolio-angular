ae4ea8c12b1b339bd115fc71f10f0b806d4375ab
sagar_patil <sagarpatil35137@gmail.com>
2024-12-09 21:46:50 +0530

    Added view more section in projects

--- src/app/components/my-projects/my-projects.component.ts
+++ src/app/components/my-projects/my-projects.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,11 +9,29 @@ import { RouterModule } from '@angular/router';
   styleUrl: './my-projects.component.scss',
 })
 export class MyProjectsComponent {
+  public isViewAll: boolean = false;
+  @ViewChild('projectsSection') projectsSection!: ElementRef;
 
-  public projectsDetails = [
+  public projectsDetails: Array<any> = [
     { image: '/assets/images/blog-web.png', title: 'Blogs web app', tools: 'Angular 16, Tailwind, NestJs, MongoDB', link: 'https://coding-blogs-seven.vercel.app/blogs' },
     { image: '/assets/images/gym-web.jpg', title: 'Gym web app', tools: 'Angular 19, Tailwind', link: 'https://fitnessclub-project.web.app/' },
     { image: '/assets/images/gallery-web.png', title: 'Image gallery web', tools: 'Angular 16, Bootstrap', link: 'https://snapify-cyan.vercel.app/' },
+  ];
+
+  private allProjects: Array<any> = [
+    { image: '/assets/images/music-web.png', title: 'Music web app', tools: 'Angular 14, Bootstrap', link: 'https://sagarpatil-17.github.io/freshmusic/' },
+    { image: '/assets/images/responsive-web.png', title: 'Responsive nav & hero', tools: 'Angular 16, Tailwind', link: 'https://responsive-nav-hero.vercel.app/' },
   ]
+
+  public viewAll() {
+    this.isViewAll = !this.isViewAll;
+
+    if (this.isViewAll) {
+      this.projectsDetails.push(...this.allProjects);
+    } else {
+      this.projectsDetails.splice(3, this.allProjects.length);
+      this.projectsSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 }
